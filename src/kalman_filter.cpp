fde5714d557d178fd09eb265b1c238c04f42f60d
Terry Lu <terrylu87@gmail.com>
2018-06-13 16:29:34 +0800

    beta

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -1,10 +1,12 @@
 #include "kalman_filter.h"
+#include "tools.h"
 
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
 
-// Please note that the Eigen library does not initialize 
+// Please note that the Eigen library does not initialize
 // VectorXd or MatrixXd objects with zeros upon creation.
+const float PI2 = 2 * M_PI;
 
 KalmanFilter::KalmanFilter() {}
 
@@ -25,6 +27,9 @@ void KalmanFilter::Predict() {
   TODO:
     * predict the state
   */
+	x_ = F_ * x_;
+	MatrixXd Ft = F_.transpose();
+	P_ = F_ * P_ * Ft + Q_;
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
@@ -32,6 +37,19 @@ void KalmanFilter::Update(const VectorXd &z) {
   TODO:
     * update the state by using Kalman Filter equations
   */
+	VectorXd z_pred = H_ * x_;
+	VectorXd y = z - z_pred;
+	MatrixXd Ht = H_.transpose();
+	MatrixXd S = H_ * P_ * Ht + R_;
+	MatrixXd Si = S.inverse();
+	MatrixXd PHt = P_ * Ht;
+	MatrixXd K = PHt * Si;
+
+	//new estimate
+	x_ = x_ + (K * y);
+	long x_size = x_.size();
+	MatrixXd I = MatrixXd::Identity(x_size, x_size);
+	P_ = (I - K * H_) * P_;
 }
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
@@ -39,4 +57,46 @@ void KalmanFilter::UpdateEKF(const VectorXd &z) {
   TODO:
     * update the state by using Extended Kalman Filter equations
   */
+
+    float px, py, vx, vy;
+    px = x_[0];
+    py = x_[1];
+    vx = x_[2];
+    vy = x_[3];
+
+    float rho, phi, rho_dot;
+    rho = sqrt(px*px + py*py);
+    phi = atan2(py, px);  // returns values between -pi and pi
+
+    // if rho is very small, set it to 0.0001 to avoid division by 0 in computing rho_dot
+    if(rho < EPSILON){
+        rho = EPSILON;
+    }
+
+    rho_dot = (px * vx + py * vy) / rho;
+
+    VectorXd z_pred = VectorXd(3);
+    z_pred << rho, phi, rho_dot;
+
+	VectorXd y = z - z_pred;
+
+    if(y(1) > M_PI){
+        y(1) -= PI2;
+    }
+
+    if(y(1) < -M_PI){
+        y(1) += PI2;
+    }
+
+	MatrixXd Ht = H_.transpose();
+	MatrixXd S = H_ * P_ * Ht + R_;
+	MatrixXd Si = S.inverse();
+	MatrixXd PHt = P_ * Ht;
+	MatrixXd K = PHt * Si;
+
+	//new estimate
+	x_ = x_ + (K * y);
+	long x_size = x_.size();
+	MatrixXd I = MatrixXd::Identity(x_size, x_size);
+	P_ = (I - K * H_) * P_;
 }
--- src/tools.cpp
+++ src/tools.cpp
@@ -5,6 +5,9 @@ using Eigen::VectorXd;
 using Eigen::MatrixXd;
 using std::vector;
 
+const float EPS = 0.0001;
+const float EPS2 = 0.0000001;
+
 Tools::Tools() {}
 
 Tools::~Tools() {}
@@ -15,6 +18,29 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
   TODO:
     * Calculate the RMSE here.
   */
+    VectorXd rmse(4);
+	rmse << 0,0,0,0;
+	if(estimations.size() == 0
+	  || estimations.size() != ground_truth.size()){
+	    cout << "error input" << endl;
+	    return rmse;
+	}
+    VectorXd squared_residuals = rmse;
+    VectorXd c = rmse;
+
+	//accumulate squared residuals
+    unsigned int i;
+	for(i=0; i < estimations.size(); ++i){
+        // ... your code here
+        c = estimations[i] - ground_truth[i];
+        c = c.array()*c.array();
+		squared_residuals = squared_residuals + c;
+	}
+    rmse = squared_residuals / estimations.size();
+	rmse = rmse.array().sqrt();
+
+	//return the result
+	return rmse;
 }
 
 MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
@@ -22,4 +48,29 @@ MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
   TODO:
     * Calculate a Jacobian here.
   */
+	MatrixXd Hj(3,4);
+	//recover state parameters
+	float px = x_state(0);
+	float py = x_state(1);
+	float vx = x_state(2);
+	float vy = x_state(3);
+
+	//TODO: YOUR CODE HERE
+
+	//check division by zero
+    if (fabs(px) < EPSILON and fabs(py) < EPSILON){
+        px = EPSILON;
+        py = EPSILON;
+    }
+
+	//compute the Jacobian matrix
+	float square_xy = px*px+py*py;
+	float sqrt_xy = sqrt(square_xy);
+	float sqrt32_xy = square_xy * sqrt_xy;
+
+	Hj << px/sqrt_xy, py/sqrt_xy, 0, 0,
+	      -py/square_xy, px/square_xy, 0, 0,
+	      py*(vx*py-vy*px)/sqrt32_xy, px*(vy*px-vx*py)/sqrt32_xy, px/sqrt_xy,py/sqrt_xy;
+
+	return Hj;
 }
